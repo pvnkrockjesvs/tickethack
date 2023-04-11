f129531fd53c80b0ee7329318a7eb17a66b4df48
Sandrine <127969652+SandrineFial@users.noreply.github.com>
2023-04-11 12:19:03 +0200

    Save pages html

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,23 @@
-let notFound = `
-<p id="ligneverte"><img src="images/notfound.png" alt="Search" height="200"></p>
-<p>No Trip found</p>`
+document.querySelector("#buttonSearch").addEventListener('click', function(){
+    // envoie le resultat de la recherche dans boite result
+    let result = ''
+    let data = true
+    if(data) {
+        result = `
+        `
+    } else {
+        result = `
+        <p id="ligneverte"><img src="images/notfound.png" alt="Search" height="200"></p>
+        <p>No Trip found</p>`
+    }
+    return document.querySelector("#resultSearch").innerHTML = result
+})
+
+
+/*
+Result Booking
+
+            <p>My bookings</p>
+            <div id="listBooking"></div>
+            <p class="colorvert">Enjoy your trajets with Tickethack!</p>
+*/
