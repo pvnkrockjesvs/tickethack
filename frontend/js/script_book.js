917d8fb8eda6c5985e70713ae0df8f9705a77533
Sandrine <fialon.sandrine@gmail.com>
2023-04-12 15:10:02 +0200

    Script de recherche màjr et script book

--- frontend/js/script_book.js
+++ frontend/js/script_book.js
@@ -1,4 +1,5 @@
 fetch('http://localhost:3000/tickets/booked').then(response => response.json()).then(books => {
+    console.log(books)
 if (books.result === false) {
         document.querySelector('#resultBooking').innerHTML += `            
         <p>No booking yet.</p>
@@ -11,15 +12,17 @@ else {
             let ticket = books.booked[i].trip[0]
             let date = new Date(ticket.date)
             let diff = moment.duration(date - moment()).humanize()
-
-            listeAffiche += `
-            <div class="listeSearch">
-                <div id="listeDepArriv">${ticket.departure} &rsaquo; ${ticket.arrival}</div>
-                <div id="listeHeure">${dateAvantDix(date.getHours())}h${dateAvantDix(date.getMinutes())}</div>
-                <div id="listePrice">${ticket.price}€</div>
-                <div id="listeDep"><strong>Departure in ${diff}</strong></div>
-            </div>
-            `
+            if(date - moment() < 0) {}
+            else {
+                listeAffiche += `
+                <div class="listeSearch">
+                    <div id="listeDepArriv">${ticket.departure} &rsaquo; ${ticket.arrival}</div>
+                    <div id="listeHeure">${dateAvantDix(date.getHours())}h${dateAvantDix(date.getMinutes())}</div>
+                    <div id="listePrice">${ticket.price}€</div>
+                    <div id="listeDep"><strong>Departure in ${diff}</strong></div>
+                </div>
+                `
+            }
         }
         listeAffiche += '<div id="lignenoire"></div><p class="colorvert">Enjoy your travels with Tickethack!</p>'
         document.querySelector('#resultBooking').innerHTML = listeAffiche
